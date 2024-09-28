857435b465ab4a5c530b7d096e9e05d99f1c8193
Manu Corujo <manucorujo@gmail.com>
2024-09-28 14:01:37 +0200

    basic plot
    still a lot of improvements to do

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -1,5 +1,8 @@
-import requests
+import matplotlib.pyplot as plt
 import pandas as pd
+import requests
+import seaborn as sns
+
 
 API_URL = "https://archive-api.open-meteo.com/v1/archive"
 COORDINATES = {
@@ -13,6 +16,7 @@ END_DATE = "2020-12-31"
 
 
 def get_data():
+    cities_values = {}
     variables_str = ",".join(VARIABLES)
     for city, coords in COORDINATES.items():
         params = {
@@ -41,16 +45,53 @@ def get_data():
                 # Convert 'time' column to datetime format
                 df["time"] = pd.to_datetime(df["time"])
 
-                print(f"Data for {city}:")
-                print(df.head())
+                cities_values[city] = df
             else:
                 print(f"No daily data found for {city}.")
         else:
             raise Exception(f"Error{r.status_code}: {r.text}")
+    return cities_values
+
+
+def plot_city_weather(df, city):
+    # lineplot for temperature
+    sns.lineplot(
+        x="time",
+        y="temperature_2m_mean",
+        data=df,
+        label="Temperature (°C)",
+        color="red",
+    )
+
+    # lineplot for precipitation
+    sns.lineplot(
+        x="time",
+        y="precipitation_sum",
+        data=df,
+        label="Precipitation (mm)",
+        color="blue",
+    )
+
+    # lineplot for wind speed
+    sns.lineplot(
+        x="time",
+        y="wind_speed_10m_max",
+        data=df,
+        label="Wind Speed (km/h)",
+        color="green",
+    )
+
+    plt.title(f"Weather Data for {city}")
+    plt.xlabel("Time")
+    plt.ylabel("Temperature (°C) / Wind Speed (km/h) / Precipitation (mm)")
+
+    plt.show()
 
 
 def main():
-    get_data()
+    dfs = get_data()
+    for city, df in dfs.items():
+        plot_city_weather(df, city)
 
 
 if __name__ == "__main__":
